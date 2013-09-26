510b7d1a8e63886b2d28d63107d7c012f5dc3b69
Apprentice <apprentice@dbc24.local>
2013-09-26 12:13:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MizeCraft::Application.config.secret_token = '350374c267c31354097a372414d6a6a430841acdb392745fdfda3f619ac03f3e0ad23cab2697d043dba8d1ce5a09eb125bf480936acbe42f49c75cbd21c2ce33'
