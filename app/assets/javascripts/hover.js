6d71b8d8d58d60261ff9bb8b7d977a48f962311d
Apprentice <apprentice@dbc2001.local>
2013-09-27 19:48:01 -0700

    created tooltip in jui

--- app/assets/javascripts/hover.js
+++ app/assets/javascripts/hover.js
@@ -1,24 +1,67 @@
+
 $(function() {
 
-    $('h1').simpletip({
-      // config properties
-      content: "I'm an h1 tag",
-      fixed: false
-    });
+  $('h1').tooltip({content: "This is an H1 Tag" ,
+    items: 'h1', track: true} )
 
-    $('p').simpletip({
-      // config properties
-      content: "I'm a p tag",
-      fixed: true,
-      position: top,
-      offset: [0,0]
-    });
+  $('p').tooltip({content: "This is a p Tag" ,
+    items: 'p', track: true} );
 
-     $('a').simpletip({
-      // config properties
-      content: "I'm an a tag",
-      fixed: false
-    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+
+    // $('h1').simpletip({
+    //   // config properties
+    //   content: "I'm an h1 tag",
+    //   fixed: false
+    // });
+
+    // $('p').simpletip({
+    //   // config properties
+    //   content: "I'm a p tag",
+    //   fixed: true,
+    //   position: top,
+    //   offset: [0,0]
+    // });
+
+    //  $('a').simpletip({
+    //   // config properties
+    //   content: "I'm an a tag",
+    //   fixed: false
+    // });
 //  $( "h1" ).hover(
 //   function() {
 //     $( this ).append( $( "<div> I am an h1 tag</div>" ).addClass("tooltip") );
@@ -27,4 +70,4 @@ $(function() {
 //   }
 // );
 
-});
+// });
