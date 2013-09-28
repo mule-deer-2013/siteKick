2bc1d252e45c25a7638af0b5929e3eda7c39d811
danapczynski <apczynski@gmail.com>
2013-09-28 15:34:14 -0700

    cleaned up views for MVP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,12 +6,13 @@ class PagesController < ApplicationController
 
   def create
     @page = Page.create(params[:page])
-
+    PageTest.create(page_id: @page.id)
     redirect_to page_path(@page)
   end
 
   def show
     @page = Page.find(params[:id])
+    @test = PageTest.where(page_id: @page.id).last
     @page.title
   end
 
