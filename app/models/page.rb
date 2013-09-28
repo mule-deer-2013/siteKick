c3516097513fac9d66a4bcd1a6b1bf1874f2f245
Apprentice <apprentice@dbc24.local>
2013-09-27 22:00:16 -0700

    refactor to DRY code

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,22 +5,76 @@ require 'open-uri'
 class Page < ActiveRecord::Base
   attr_accessible :content, :original_url
 
+  before_create :get_content
+
+
   def get_content
     url = self.original_url
-    nokogiri = Nokogiri::HTML(open(url))
-    self.content = nokogiri.inner_html
+    self.content = Nokogiri::HTML( open(url) ).css('article').inner_html
+    save if !new_record?  #Temporal: Please keep it because it helps me when we call it in the console 
+  end
+
+  def nokogiri
+    @nokogiri ||= Nokogiri::HTML( content ) # .tap do |obj|
+      # Rails.logger.debug( "**** Nokogiri: #{obj.inspect}" )  # debug in rails (.tap...)
+    # end
   end
 
   def title
-    url = self.original_url
-    nokogiri = Nokogiri::HTML(open(url))
-    nokogiri.at_css("title").text
+    nokogiri.css("title").text
   end
 
-  def h1
-    url = self.original_url
-    nokogiri = Nokogiri::HTML(open(url))
+  def h1_text
     nokogiri.css("h1").text
   end
+
+  def word_count #400-600?
+    word = nokogiri.css("p").text
+    array_words = word.split
+    array_words.length  #aprox based on p tags
+  end
+
+  def number_of_h2
+    word = (nokogiri.css("h2").count)/2
+    word
+  end
+
+  def number_of_h3
+    word = (nokogiri.css("h3").count)/2
+    word
+  end
+
+
+  # def brainstorming_evaluate_words_on_page
+  #   @most_common_words = []
+  #   @number_of_header_tags
+  #   @self_referring_links
+  #   @broken_links
+  #   @outgoing_links
+  #   @font_size_for_p_tags
+  #   @avg_paragraph_length
+  #   @keywords_in_url
+  #   @title_length
+  #   @keywords_in_meta_description
+  #   @meta_description_length
+  #   @meta_title
+  #   @keyword_density #percentage of keywords on page
+  #   @keyword_variations
+  #   @starts_with_text?
+  #   @number_of_images
+  #   @h1_and_h2_and_h3?
+  #   @number_of_h1s
+  #   @keywords_in_headings
+  #   @alt_text_in_images?
+  #   @text_versus_html #25-70% text
+  #   @page_load_speed #?
+
+  # end
+
+
+  
+
+
+
 
 end
