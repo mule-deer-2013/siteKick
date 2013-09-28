d73d6c8a2e0dab85bbb263cf0cd3c33166fe82c2
Eva Zamudio <evazamudio@narshe.local>
2013-09-28 15:57:20 -0700

    add new two fields to our table

--- db/migrate/20130926224212_create_pages.rb
+++ db/migrate/20130926224212_create_pages.rb
@@ -3,6 +3,8 @@ class CreatePages < ActiveRecord::Migration
     create_table :pages do |t|
       t.text :content
       t.text :original_url
+      t.text :title
+      t.text :meta
       t.timestamps
     end
   end
