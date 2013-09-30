6df251fa01119dd1028dcd301c41afc0ebe05480
Luisa <Emmi.Beck@gmail.com>
2013-09-29 18:27:55 -0700

    eight passing user tests for user creation and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should allow_mass_assignment_of :email }
+  it { should allow_mass_assignment_of :password_confirmation }
+  it { should allow_mass_assignment_of :password }
+  it { should callback(:encrypt_password).before(:save) }
+  it { should validate_confirmation_of :password }
+  it { should validate_presence_of :password }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+
+
 end
