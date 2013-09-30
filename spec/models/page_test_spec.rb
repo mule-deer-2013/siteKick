46c610807fd92dd2574fc9be46120b100e74f6c5
Luisa <Emmi.Beck@gmail.com>
2013-09-29 18:10:51 -0700

    four green page_test tests

--- spec/models/page_test_spec.rb
+++ spec/models/page_test_spec.rb
@@ -5,6 +5,7 @@ describe PageTest do
   it { should belong_to :page}
   it { should allow_mass_assignment_of :page_id }
   it { should callback(:run_test_suite).after(:create)}
+  it { should serialize(:test_results).as(Hash)}
 
 
 end
