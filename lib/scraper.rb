95873a8b9de303a0901668fc5540b6e72c17d5a9
Apprentice <apprentice@dbc24.local>
2013-09-30 19:32:15 -0700

    moved Page methods to lib/scraper and created scraper_spec.rb

---
+++ lib/scraper.rb
@@ -0,0 +1,145 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'uri'
+require 'ruby-debug'
+
+module Scraper    
+
+  def get_content
+    post_id = "#" + open_url.inner_html.match(/post-\d+/).to_s
+    # self.content = open_url.css(post_id).inner_html
+    self.content = open_url.css('article').inner_html
+    self.title = open_url.css('title').text
+    self.meta = open_url.css('meta').text
+  end
+
+  def open_url
+    @content ||= Nokogiri::HTML( open("#{self.original_url}") )
+  end
+
+  module Result
+    def article_nokogiri
+      @article_nokogiri ||= Nokogiri::HTML( self.content )
+    end
+
+    def remove_scripts(nokogiri_content)
+      article_nokogiri_content.xpath("//script").remove
+    end
+
+    def h1_text
+      if number_of_h1 < 2
+        article_nokogiri.css("h1").text
+      else
+        article_nokogiri.css("h1").first.text
+      end
+    end 
+
+    def word_count
+      words = article_nokogiri.text
+      words.split.length
+    end
+
+    def number_of_h1
+     article_nokogiri.css("h1").count
+    end
+
+    def number_of_h2
+      article_nokogiri.css("h2").count
+    end
+
+    def number_of_h3
+     article_nokogiri.css("h3").count
+    end
+
+    def remove_stop_words
+      text = article_nokogiri.css("p")
+      text = text.text
+      text = text.downcase!
+      array_text = text.scan(/\w+/)
+      stop_words = ["0", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "re", 'd', 'll', 'm', 't', 's', 've', 'weren', 'haven', 'aren', 'don', 'shouldn', 'shan', 'mustn']
+      array_text.reject! { |word| word.to_i != 0 }
+      array_text.reject! { |word| stop_words.include?(word) }
+      array_text
+    end
+
+    def keyword_frequency
+      clean_text = remove_stop_words
+      freqs = Hash.new(0)
+      clean_text.each { |word| freqs[word] +=1 }
+      freqs = freqs.sort_by {|x,y| y }
+      freqs.reverse!
+      frequent_words_array = freqs.each { |word, freq| word+ ' '+freq.to_s}
+      frequent_words_array[0...5]
+    end
+
+    def number_of_images
+      article_nokogiri.css("img").count
+    end
+
+    def start_with_text?
+      first_word = article_nokogiri.text.split.first
+      !content.match(first_word).pre_match.include?("img")
+    end
+
+    def image_alt_tags
+      article_nokogiri.css('img').map{ |i| i['alt'] }
+    end
+
+    def avg_para_length
+      paragraphs = article_nokogiri.css("p").map { |para| para.text }
+      para_lengths = paragraphs.map { |para| para.length }
+      para_lengths.inject(:+)/paragraphs.length
+    end
+
+    def text_to_html_ratio
+      text_length = article_nokogiri.text.length
+      html_length = article_nokogiri.css('html').inner_html.length
+      text_length.to_f / html_length.to_f
+    end
+
+    def list_of_links
+      links = article_nokogiri.css('a').map {|link| link['href']}
+    end
+
+    # This method does not returning the intended metric. 
+    # 'Self-referring links' refers to other pages within the blog—not the same page.
+    def self_referring_links
+      links = article_nokogiri.css('a').map {|link| link['href']}
+      links.select { |link| link.include?(original_url) }
+    end
+
+  # TO DO: Discuss negative tests with team. Wordpress and Tumblr
+  # seem to have built-in redirects??
+    def broken_links_due_syntax
+      links = article_nokogiri.css('a').map {|link| link['href']} # output => array of string urls
+      broken_syntax_links = []
+      links.each do |link|
+        url = URI.parse(link)
+        url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+        broken_syntax_links << url if false
+      end
+      broken_syntax_links
+    end
+
+
+
+    def broken_links_code
+      links = article_nokogiri.css('a').map {|link| link['href']} # output => array of string urls
+      array_code = links.map do |link|
+        url = URI.parse(link)
+        req = Net::HTTP::Get.new(url.path)
+        res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
+        res.code
+      end
+       #Output: array with code response for each link ex.200
+
+      # PENDING --->
+           # - Need to add Queuing Jobs(Call Back): https://github.com/collectiveidea/delayed_job
+      handle_asynchronously :delivery
+    end
+
+
+  end
+
+end
---
+++ spec/lib/scraper_spec.rb
@@ -0,0 +1,146 @@
+require 'spec_helper'
+class MyPage
+  attr_accessor :content, :title, :meta
+  include Scraper
+  include Scraper::Result
+end
+
+describe Scraper do
+  let(:page) { MyPage.new }
+  let(:nokogiri) {
+    mock(:nokogiri, :inner_html => '', :css => mock(:css, :inner_html => ',', :text => ''))
+  }
+  context "#get_content" do
+    it "scrapes the original url" do
+      page.stub(:open_url) { nokogiri }
+      page.get_content
+    end
+  end
+
+  context "#article_nokogiri" do
+    it "creates a nokogiri html object with the content" do
+      page.stub(:content) { 'page content' }
+      Nokogiri.should_receive(:HTML).with(page.content)
+      page.article_nokogiri
+    end
+  end
+end
+
+describe Scraper::Result do
+  let(:page) { FactoryGirl.create(:page) }
+  context "#h1_text" do
+    context "with a single h1 tag" do
+      it "returns the text of an h1 tag" do
+        expect(page.h1_text).to eq "The Power of Equality"
+      end
+    end
+    context "with multiple h1 tags" do
+      let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/too_many_h1s.html') }
+      it "returns the text of the first h1 tag" do
+        expect(page.h1_text).to eq "The Power of Wishful Thinking"
+      end
+    end
+    context "with zero h1 tags" do
+      let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/zero_h1s.html') }
+      it "returns an empty string" do
+        expect(page.h1_text).to eq ""
+      end
+    end
+  end
+
+  context "#word_count" do
+    it "returns the word count of the article body" do
+      expect(page.word_count).to be 563
+    end
+  end
+
+  context "#number_of_h1" do
+    it "returns the number of h1 tags in the article body" do
+      expect(page.number_of_h1).to be 1
+    end
+  end
+
+  context "#number_of_h2" do
+    it "returns the number of h2 tags in the article body" do
+      expect(page.number_of_h2).to be 1
+    end
+  end
+
+  context "#number_of_h3" do
+    it "returns the number of h3 tags in the article body" do
+      expect(page.number_of_h3).to be 1
+    end
+  end
+
+  context "#remove_stop_words" do
+    it "returns an array of strings" do
+      expect(page.remove_stop_words).to be_an_instance_of Array
+      expect(page.remove_stop_words.first).to be_an_instance_of String
+    end
+
+    it "filters common words from the article body" do
+      expect(page.remove_stop_words).not_to include('the')
+    end 
+  end
+
+  context "#keyword_frequency" do
+    it "returns a nested array of words and their frequencies" do
+      expect(page.keyword_frequency.first).to eq ['board', 6]
+    end
+  end
+
+  context "#number_of_images" do
+    it "returns the number of image tags on the page" do
+      expect(page.number_of_images).to be 1
+    end
+  end
+
+  context "#start_with_text?" do
+    context "when the page starts with text" do
+      it "returns true" do
+        expect(page.start_with_text?).to be true
+      end
+    end
+
+    context "when the page starts with an image" do
+      let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/starts_with_image.html') }
+      it "returns false" do
+        expect(page.start_with_text?).to be false
+      end
+    end
+  end
+
+  context "#image_alt_tags" do
+    context "when the page contains multiple images" do
+      let (:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/multiple_images.html')}
+      it "should return an array of alt tags from images" do
+        expect(page.image_alt_tags).to eq ["image1", "image2", "image3"]
+      end
+    end
+
+    context "when the page contains a singler image" do
+      it "should return a single-item array" do
+        expect(page.image_alt_tags).to eq ["image"]
+      end
+    end
+  end
+
+  context "#avg_para_length" do
+    it "should return the average length of all text paragraphs" do
+      expect(page.avg_para_length).to be 241
+    end
+  end
+
+  context "#text_to_html_ratio" do
+    it "should return the ratio of text characters to html characters" do
+      expect((page.text_to_html_ratio * 100000).to_i). to be 71627
+    end
+  end
+
+  context "list_of_links" do
+    it "should return a list of all links found on the page" do
+      expect((page.list_of_links).length) == 6
+      expect((page.list_of_links).first).to eq "http://editorreverse.tumblr.com/post/58401824401/the-power-of-equality#notes"
+    end
+  end
+end
