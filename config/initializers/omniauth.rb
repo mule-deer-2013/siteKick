5d657ccd66dd32872beca1da5a06ec744f8e2463
danapczynski <apczynski@gmail.com>
2013-10-02 11:03:50 -0700

    pulled middleware handoff out of omniauth conditional

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
-if Rails.env != "production"
+unless Rails.env.production?
   require 'dotenv'
   Dotenv.load
+end
 
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :tumblr, ENV['TUMBLR_KEY'], ENV['TUMBLR_SECRET']
-  end
-
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :tumblr, ENV['TUMBLR_KEY'], ENV['TUMBLR_SECRET']
 end
