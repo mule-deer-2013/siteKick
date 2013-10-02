f23d9db968ff67940ccfeb474f791209aeabeaba
Alex Fiore <writefiore@gmail.com>
2013-10-02 15:17:24 -0700

    removed debugger

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < ApplicationController
   
   def create
-    debugger
-    puts "*"*80
-    puts params
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
