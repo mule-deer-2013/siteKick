2cc4e68c52499f846dfe852fd84420a50204cb22
danapczynski <apczynski@gmail.com>
2013-10-02 10:50:22 -0700

    experimenting with omniauth for heroku push

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 if Rails.env != "production"
   require 'dotenv'
   Dotenv.load
-end
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :tumblr, ENV['TUMBLR_KEY'], ENV['TUMBLR_SECRET']
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :tumblr, ENV['TUMBLR_KEY'], ENV['TUMBLR_SECRET']
+  end
+
 end
