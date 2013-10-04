0f9f1a337a71765a33bae3fea2509af6e84900f1
Apprentice <apprentice@dbc2001.local>
2013-10-03 17:17:24 -0700

    fixed tests that failed after content edit

--- spec/lib/analyzer_spec.rb
+++ spec/lib/analyzer_spec.rb
@@ -14,7 +14,7 @@ describe Analyzer do
     context "with one keyword in the title" do
      
       it "returns the appropriate message" do
-        expect(page_test.title_includes_keywords_test).to eq "Of these words, we found the word \"equality\" in your title tag. That's a good thing!"
+        expect(page_test.title_includes_keywords_test).to eq "The keyword \"equality\" appears in your title tag."
       end
 
       it "passes" do
@@ -27,7 +27,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
       
       it "returns the appropriate message" do
-        expect(page_test.title_includes_keywords_test).to eq "None of the words that occur most often in your post occur in your title tag."
+        expect(page_test.title_includes_keywords_test).to eq "None of your keywords occur in your title tag."
       end
 
       it "fails" do
@@ -40,7 +40,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
      
       it "returns the appropriate message" do
-        expect(page_test.title_includes_keywords_test).to eq "We found several of these words in your title tag. Way to go!" 
+        expect(page_test.title_includes_keywords_test).to eq "Several of your keywords appear in your title tag." 
       end
 
       it "passes" do
@@ -55,7 +55,7 @@ describe Analyzer do
         let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/keyword_board_in_url.html') }
         let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
         it "returns the appropriate message" do
-          expect(page_test.url_includes_keywords_test).to eq "We found the word \"board\" in your URL. That's a good thing!"
+          expect(page_test.url_includes_keywords_test).to eq "The word \"board\" appears in your URL."
         end
 
         it "passes" do
@@ -67,7 +67,7 @@ describe Analyzer do
         let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/keyword_equality_in_url.html') }
         let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
         it "returns the appropriate message" do
-          expect(page_test.url_includes_keywords_test).to eq "We also found \"equality\" in your page's URL."
+          expect(page_test.url_includes_keywords_test).to eq "The word \"equality\" appears in your URL."
         end
 
         it "passes" do
@@ -78,7 +78,7 @@ describe Analyzer do
 
     context "with zero keywords in the url" do
       it "returns the appropriate message" do
-        expect(page_test.url_includes_keywords_test).to eq "None of your keywords are present in your post's URL. Customizing your URL to include your keywords could help direct search traffic toward your content."
+        expect(page_test.url_includes_keywords_test).to eq "None of your keywords were found in your post's URL."
       end
 
       it "fails" do
@@ -91,7 +91,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
      
       it "returns the appropriate message" do
-        expect(page_test.url_includes_keywords_test).to eq "We found several keywords in your post's URL. Nice work!" 
+        expect(page_test.url_includes_keywords_test).to eq "Several of your keywords were found in your post's URL." 
       end
 
       it "passes" do
@@ -106,7 +106,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.word_count_test).to eq "Your piece is so short that search engines may not consider it very important. Consider trying to add additional content."
+        expect(page_test.word_count_test).to eq "Your piece is so short that search engines may not consider it very important."
       end
 
       it "fails" do
@@ -116,7 +116,7 @@ describe Analyzer do
 
     context "with 301-600 words" do
       it "returns the appropriate message" do
-        expect(page_test.word_count_test).to eq "In terms of length, this piece is right in the pocket of what search engines are looking for."
+        expect(page_test.word_count_test).to eq "This piece falls into a word count range that search engines generally consider to be the optimal length."
       end
 
       it "passes" do
@@ -128,7 +128,7 @@ describe Analyzer do
       let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/lot_of_words.html') }
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
       it "returns the appropriate message" do
-        expect(page_test.word_count_test).to eq "Your piece is a bit longer than what is considered optimal for search (~600 words). You're still within a reasonable length, but it might be worth keeping an eye on your word count."
+        expect(page_test.word_count_test).to eq "Your piece is a bit longer than what is considered optimal for search, although you're still within a reasonable length."
       end
 
       it "passes" do
@@ -140,7 +140,7 @@ describe Analyzer do
       let(:page) { FactoryGirl.create(:page, original_url: 'spec/factories/dummy_html/too_many_words.html') }
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
       it "returns the appropriate message" do
-        expect(page_test.word_count_test).to eq "Your piece is quite a bit longer than the recommended maximum of 600 words. Consider refocusing on your chosen topic and eliminating unnecessary content."
+        expect(page_test.word_count_test).to eq "Your piece is longer than the recommended maximum of 600 words."
       end
 
       it "fails" do
@@ -155,7 +155,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.h1_presence_test).to eq "You have more than one h1 tag in your post, which may cause search engines to penalize your content."
+        expect(page_test.h1_presence_test).to eq "You have more than one &lt;h1&gt; tag in your post, which may cause search engines to penalize your content."
       end
 
       it "fails" do
@@ -168,7 +168,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.h1_presence_test).to eq "You don't have any h1 tags in your piece. We strongly advise that you provide a title for your post in an \<h1\> tag."
+        expect(page_test.h1_presence_test).to eq "You don't have any &lt;h1&gt; tags in your piece."
       end
 
       it "fails" do
@@ -178,7 +178,7 @@ describe Analyzer do
 
     context "with one h1 tag" do
       it "returns the appropriate message" do
-        expect(page_test.h1_presence_test).to eq "You have one h1 tag, which is exactly what search engines want to see."
+        expect(page_test.h1_presence_test).to eq "You have one &lt;h1&gt; tag, which is exactly what search engines want to see."
       end
 
       it "passes" do
@@ -194,7 +194,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.h1_keywords_test).to eq "None of your keywords are present in your post's h1 tag. Customizing your h1 to include your keywords could help direct search traffic toward your content."
+        expect(page_test.h1_keywords_test).to eq "None of your keywords are present in your post's &lt;h1&gt; tag."
       end
       it "fails" do
         expect(page_test.test_results[:h1_keyword_result]).to be false
@@ -203,7 +203,7 @@ describe Analyzer do
 
     context "with one keyword in the first h1" do
       it "returns the appropriate message" do
-        expect(page_test.h1_keywords_test).to eq "We found the word \"equality\" in your h1 tag. That's a good thing!"
+        expect(page_test.h1_keywords_test).to eq "We found the word \"equality\" in your &lt;h1&gt; tag."
       end
       it "passes" do
         expect(page_test.test_results[:h1_keyword_result]).to be true
@@ -215,7 +215,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.h1_keywords_test).to eq "We found several keywords in your h1 tag. Nice work!"
+        expect(page_test.h1_keywords_test).to eq "We found several keywords in your &lt;h1&gt; tag."
       end
       it "passes" do
         expect(page_test.test_results[:h1_keyword_result]).to be true
@@ -227,10 +227,10 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
       
       it "returns the appropriate message" do
-        expect(page_test.h1_keywords_test).to eq "An h1 tag is an excellent opportunity to show search engines what your piece is about. Add one to the top of your piece and use one or more keywords to send a strong message about your content."
+        expect(page_test.h1_keywords_test).to eq "You don't currently have any &lt;h1&gt; tags in your post, so this test did not evaluate. Consider adding an &lt;h1&gt; tag to help search engines and users quickly recognize what your content is about."
       end
-      it "fails" do
-        expect(page_test.test_results[:h1_keyword_result]).to be false
+      it "does not evaluate" do
+        expect(page_test.test_results[:h1_keyword_result]).to be :not_evaluated
       end
     end
   end 
@@ -252,7 +252,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.image_alt_tags_presence_test).to eq "At least one of your image tags doesn't include an 'alt' description. When used responsibly, these descriptions are an excellent way to tell search engines what your page is about."
+        expect(page_test.image_alt_tags_presence_test).to eq "At least one of your image tags doesn't include an 'alt' description."
       end
 
       it "fails" do
@@ -265,7 +265,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.image_alt_tags_presence_test).to eq "Your image tags don't currently include 'alt' descriptions. When used responsibly, these descriptions are an excellent way to tell search engines what your page is about."
+        expect(page_test.image_alt_tags_presence_test).to eq "Your image tags don't currently include 'alt' descriptions."
       end
 
       it "fails" do
@@ -278,11 +278,11 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.image_alt_tags_presence_test).to eq "Images offer you an opportunity to share more information with search engines about what your content is about (and hopefully help to make your page look cool) but if you don't want them in your content, don't worry about it!"
+        expect(page_test.image_alt_tags_presence_test).to eq "Your post doesn't have any images, so we could not test them for the presence of 'alt' descriptions."
       end
 
-      it "passes" do
-        expect(page_test.test_results[:alt_tags_presence_result]).to be true
+      it "does not evaluate" do
+        expect(page_test.test_results[:alt_tags_presence_result]).to be :not_evaluated
       end
     end
   end 
@@ -290,7 +290,7 @@ describe Analyzer do
   describe "image alt tags keywords test" do
     context "when the page doesn't have any keywords in its alt descriptions" do
       it "returns the appropriate message" do
-        expect(page_test.image_alt_tags_keywords_test).to eq "You are not currently using any of your keywords in your 'alt' descriptions, which is a missed opportunity to help tell search engines what your page is about."
+        expect(page_test.image_alt_tags_keywords_test).to eq "You are not currently using any of your keywords in your 'alt' descriptions."
       end
 
       it "fails" do
@@ -303,7 +303,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.image_alt_tags_keywords_test).to eq "Your keywords occur so commonly in your 'alt' descriptions that search engines might think that you're attempting to cheat the system. In order to avoid being penalized, examine your 'alt' descriptions and consider dialing back on your keyword usage."
+        expect(page_test.image_alt_tags_keywords_test).to eq "Your keywords occur so commonly in your 'alt' descriptions that search engines might think that you're attempting to cheat the system."
       end
 
       it "fails" do
@@ -316,7 +316,7 @@ describe Analyzer do
       let(:page_test) { FactoryGirl.create(:page_test, page_id: page.id) }
 
       it "returns the appropriate message" do
-        expect(page_test.image_alt_tags_keywords_test).to eq "Without overdoing it, you're averaging at least one keyword for each of your 'alt' descriptions. That's right where you want your images to be."
+        expect(page_test.image_alt_tags_keywords_test).to eq "Without overdoing it, you're averaging at least one keyword for each of your 'alt' descriptions."
       end
 
       it "passes" do
